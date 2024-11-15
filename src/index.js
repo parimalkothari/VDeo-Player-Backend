1f8c796385758e8c5222f738596243fa34a0e745
Parimal Kothari <parimalkothari60@gmail.com>
2024-11-15 12:32:10 +0530

    Custom Api Response and Api Error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import {DBConnection} from "./db/db.js";
+import app from "./app.js"
 
 DBConnection()
+.then(()=>{
+    app.listen(process.env.PORT||3000,()=>{
+        console.log(`Listening to Server at port: ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB Connection Failed",error)
+})
